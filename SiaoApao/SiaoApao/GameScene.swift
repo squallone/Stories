0a256230c9e3124191c9b6a801eff686f14a67f4
Raul Lopez Martinez <raul.lopez@ironbit.com.mx>
2016-09-06 18:23:18 -0500

    - Configuration: -Project Configuration. -Add Splash screen Image.

---
+++ SiaoApao/SiaoApao/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SiaoApao
+//
+//  Created by Raúl López on 9/6/16.
+//  Copyright (c) 2016 Raúl López. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+          backgroundColor = SKColor.blackColor()
+    }
+
+}
