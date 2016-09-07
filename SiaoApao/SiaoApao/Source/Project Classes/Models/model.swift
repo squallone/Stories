91ff6bc928d78bb259be4c82469aa931328bb088
Abdiel Soto <abdielsba@gmail.com>
2016-09-06 19:15:49 -0500

    - References: Create folders and dummy classes

---
+++ SiaoApao/SiaoApao/Source/Project Classes/Models/model.swift
@@ -0,0 +1,13 @@
+//
+//  model.swift
+//  SiaoApao
+//
+//  Created by Abdiel Soto on 9/6/16.
+//  Copyright © 2016 Raúl López. All rights reserved.
+//
+
+import UIKit
+
+class model: NSObject {
+
+}
---
+++ SiaoApao/SiaoApao/Source/Project Classes/Views/view.swift
@@ -0,0 +1,13 @@
+//
+//  view.swift
+//  SiaoApao
+//
+//  Created by Abdiel Soto on 9/6/16.
+//  Copyright © 2016 Raúl López. All rights reserved.
+//
+
+import UIKit
+
+class view: NSObject {
+
+}
