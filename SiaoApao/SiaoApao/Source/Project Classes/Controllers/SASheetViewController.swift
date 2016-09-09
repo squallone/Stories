49e220c7c77f57ecfb7163b34c3ccc895c2c2988
Abdiel Soto <abdielsba@gmail.com>
2016-09-08 23:25:13 -0500

    - Bubble scene: Add Z values

--- SiaoApao/SiaoApao/Source/Project Classes/Controllers/SASheetViewController.swift
+++ SiaoApao/SiaoApao/Source/Project Classes/Controllers/SASheetViewController.swift
@@ -13,25 +13,25 @@ class SASheetViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         /* Init self.view to suport scence, beacuse this viewController is not use XIB */
         self.view = SKView(frame: UIScreen.mainScreen().bounds)
-
+        
         
         
         //if let scene = SADayAndNightScene(fileNamed:"SADayAndNightScene") {
-        if let scene = SASurfScene(fileNamed:"SASurfScene") {
+        if let scene = SASurfScene(fileNamed:"SASceneGlobes") {
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
-        
+            
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
-        
+            
             /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
-        
+            
             skView.presentScene(scene)
         }
     }
