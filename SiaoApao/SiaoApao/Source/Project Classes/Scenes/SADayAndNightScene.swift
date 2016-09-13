9a86428f9a6e3689fdd00b66582995b398153e2e
Raul Lopez Martinez <raul.lopez@ironbit.com.mx>
2016-09-13 10:30:12 -0500

    - Scene One: Create gesture (incomplete) in first scene -Add Sun asset

--- SiaoApao/SiaoApao/Source/Project Classes/Scenes/SADayAndNightScene.swift
+++ SiaoApao/SiaoApao/Source/Project Classes/Scenes/SADayAndNightScene.swift
@@ -8,77 +8,49 @@
 
 import SpriteKit
 
-/*
- 
- // MARK: - Margin Enum
- 
- enum Margin {
- static let girlMarginX: CGFloat = 210.0
- static let girlPositionY: CGFloat = 255.0
- }
- 
- // MARK: - Scale Enum
- 
- enum Scale {
- static let girls:CGFloat = 0.8
- }
- 
- // MARK: - z Node position  Enum
- 
- enum zNodePosition {
- static let fence: CGFloat = -1
- static let tree: CGFloat = -0.7
- static let fenceGirls: CGFloat = -0.5
- static let interaction: CGFloat = 0.0
- static let square: CGFloat = 0.5
- static let sink: CGFloat = 1.0
- }*/
-
-
 class SADayAndNightScene: SABaseScene {
     
+    // MARK: - Points
+    var startYPosition: CGFloat?
+    var finishYposition: CGFloat?
+    
+    // MARK: - Properties
+    var sunSpriteNode: SKSpriteNode?
+    var moonSpriteNode: SKSpriteNode?
+    var selectedNode = SKSpriteNode()
+
+
     /* Setup your scene here */
     override func didMoveToView(view: SKView) {
         super.didMoveToView(view)
         
-        /* Create node statics */
-        /*
-         let fenceNode = SKSpriteNode(imageNamed: "fence")
-         fenceNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
-         fenceNode.anchorPoint = CGPoint(x: 0.5, y: 1.0)
-         fenceNode.zPosition = zNodePosition.fence
-         addChild(fenceNode)
-         
-         let treeNode = SKSpriteNode(imageNamed: "tree")
-         treeNode.position = CGPoint(x: size.width / 2, y: size.height / 2 + 90)
-         treeNode.anchorPoint = CGPoint(x:0.5, y:0.5)
-         treeNode.zPosition = zNodePosition.tree
-         addChild(treeNode)
-         
-         let fenceGirl = SKSpriteNode(imageNamed: "FenceGirl")
-         fenceGirl.position = CGPoint(x: Margin.girlMarginX, y: Margin.girlPositionY)
-         fenceGirl.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-         fenceGirl.setScale(Scale.girls)
-         fenceGirl.zPosition = zNodePosition.fenceGirls
-         addChild(fenceGirl)
-         
-         let fenceGirl2 = SKSpriteNode(imageNamed: "FenceGirl2")
-         fenceGirl2.position = CGPoint(x: size.width - Margin.girlMarginX, y: Margin.girlPositionY)
-         fenceGirl2.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-         fenceGirl2.setScale(Scale.girls)
-         fenceGirl2.zPosition = zNodePosition.fenceGirls
-         addChild(fenceGirl2)
-         
-         let squareNode = SKSpriteNode(imageNamed:"square")
-         squareNode.position = CGPoint(x: 0, y: size.height)
-         squareNode.anchorPoint = CGPoint(x: 0, y: 1)
-         squareNode.zPosition = zNodePosition.square
-         addChild(squareNode)
-         
-         let sinkNode = SKSpriteNode(imageNamed: "sink")
-         sinkNode.position = CGPoint(x: 0, y:0)
-         sinkNode.anchorPoint = CGPoint(x: 0, y:0)
-         sinkNode.zPosition = zNodePosition.sink
-         addChild(sinkNode)*/
+        sunSpriteNode = self.childNodeWithName("sun") as? SKSpriteNode
+        moonSpriteNode = self.childNodeWithName("moon") as? SKSpriteNode
+        
+        let gestureRecognizer = UIGestureRecognizer(target: self, action: #selector(SADayAndNightScene.handlePanFrom))
+        self.view!.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    
+    // MARK: - Gesture Actions
+    
+    func handlePanFrom(recognizer: UIPanGestureRecognizer) {
+        if recognizer.state == .Began {
+            var touchLocation = recognizer.locationInView(recognizer.view)
+            touchLocation = self.convertPointToView(touchLocation)
+        } else if recognizer.state == .Changed {
+            
+        } else if recognizer.state == .Ended {
+            
+            
+        }
+    }
+    
+    func panForTranslation(translation: CGPoint) {
+        let position = selectedNode.position
+        
+        if selectedNode.name! == sunSpriteNode || selectedNode.name! == moonSpriteNode {
+            selectedNode.position = CGPoint(x: position.x + translation.x, y: position.y + translation.y)
+        }
     }
 }
