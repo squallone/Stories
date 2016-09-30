9ea2959a97846b1f9b1960d9a4c9f10b7c5332a0
Raul Lopez Martinez <rlopezmartinez3@gmail.com>
2016-09-30 00:43:22 -0500

    - Puppet Scene :Update puppet scene

--- SiaoApao/SiaoApao/Source/Project Classes/Scenes/SAPuppetScene.swift
+++ SiaoApao/SiaoApao/Source/Project Classes/Scenes/SAPuppetScene.swift
@@ -9,9 +9,31 @@
 import SpriteKit
 
 class SAPuppetScene: SABaseScene {
+    
+    
+    // MARK: - Properties
+    private var puppetSpriteNode: SKSpriteNode?
+    private var smileShapeNode: SKShapeNode!
+    
+    let laughSound: SKAction = SKAction.playSoundFileNamed(
+        "laughing.aiff", waitForCompletion: true)
+    
+    
     /* Setup your scene here */
     override func didMove(to view: SKView) {
         super.didMove(to: view)
+        
+        /* Init Properties */
+        puppetSpriteNode = self.childNode(withName: "puppet") as? SKSpriteNode
+        
+        /* Create the smile */
+        let smileCenter = CGPoint(x: -20, y: -100)
+        let arcPath =  UIBezierPath(arcCenter: smileCenter, radius:30, startAngle: CGFloat.pi, endAngle: 0, clockwise:false)
+        
+        smileShapeNode = SKShapeNode(path: arcPath.cgPath)
+        smileShapeNode.strokeColor = UIColor.yellow
+        smileShapeNode.lineWidth = 5
+        smileShapeNode.zPosition = (puppetSpriteNode?.zPosition)! + 1
+        puppetSpriteNode?.addChild(smileShapeNode!)
     }
-    
 }
