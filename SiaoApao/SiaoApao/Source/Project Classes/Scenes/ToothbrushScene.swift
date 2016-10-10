76cf7c7bd218a528ceea9d38a7986509e6f8fc67
Raul Lopez Martinez <rlopezmartinez3@gmail.com>
2016-10-09 20:25:09 -0500

    - Scene: Brush scene complete

--- SiaoApao/SiaoApao/Source/Project Classes/Scenes/ToothbrushScene.swift
+++ SiaoApao/SiaoApao/Source/Project Classes/Scenes/ToothbrushScene.swift
@@ -10,9 +10,96 @@ import SpriteKit
 
 class ToothbrushScene: SABaseScene {
     
+    // MARK: - Properties
+    var bubblesGirlNode: SKSpriteNode?
+    var bubblesGirlTwoNode: SKSpriteNode?
+    
+    // MARK: - Action
+    var bubblesGirlNumberTouch: NSInteger = 8
+    var bubblesGirlTwoNumberTouch: NSInteger = 8
+    
+    var constantAlpha: CGFloat = (1 / 8)
+
+    
+    let brushSound: SKAction = SKAction.playSoundFileNamed(
+        "brush.wav", waitForCompletion: true)
+    
+    
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         
+        /* Init properties */
+        bubblesGirlNode = self.childNode(withName: "bubbles1") as? SKSpriteNode
+        bubblesGirlTwoNode = self.childNode(withName: "bubbles2") as? SKSpriteNode
+        
+        /* Create Bounce Action */
+        let bounceAction = SKAction.bounce(to: 1.05, duration: 0.2)
+        bubblesGirlNode?.run(SKAction.repeatForever(bounceAction))
+        bubblesGirlTwoNode?.run(SKAction.repeatForever(bounceAction))
+    }
+    
+    // MARK: - Gesture Actions
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        if let location = touches.first?.location(in: self) {
+            let touchedNode = atPoint(location)
+            
+            if touchedNode is SKSpriteNode {
+                
+                if touchedNode == bubblesGirlNode || touchedNode == bubblesGirlTwoNode {
+                    let animationTouches = touchedNode == bubblesGirlNode ? bubblesGirlNumberTouch : bubblesGirlTwoNumberTouch
+
+                    if (touchedNode.hasActions()) {
+                        touchedNode.removeAllActions()
+                    
+                        /* Calculate the new Alpha */
+                        let newAlpha = touchedNode.alpha - constantAlpha
+
+                        /* Create the action */
+                        let bubbleAnimation = SKAction.fadeAlpha(to: newAlpha,
+                                                                 duration: 0.8)
+                        let groupAction = SKAction.group([bubbleAnimation,brushSound])
+                        
+                        touchedNode.run(groupAction, completion: {
+                            if (animationTouches > 1) {
+                                let bounceAction = SKAction.bounce(to: 1.05, duration: 0.2)
+                                touchedNode.run(SKAction.repeatForever(bounceAction))
+                                
+                                /* Update Photo Touches */
+                                if touchedNode == self.bubblesGirlNode {
+                                    self.bubblesGirlNumberTouch -= 1
+                                } else {
+                                    self.bubblesGirlTwoNumberTouch -= 1
+                                }
+                                
+                                if self.checkBubbleIsTapped() {
+                                    print("Show next Button")
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    /* Check if both portrats is in finish  */
+    func checkBubbleIsTapped() -> Bool {
+        
+        var firsPortraitIsTapped: Bool = false
+        var secondPortraitIsTapped: Bool = false
+        
+        if (bubblesGirlNode?.alpha)! <= 0.0 {
+            firsPortraitIsTapped = true
+        }
+        
+        if (bubblesGirlTwoNode?.alpha)! <= 0.0 {
+            secondPortraitIsTapped = true
+        }
+        
+        let allBubbleIsTapped = (firsPortraitIsTapped && secondPortraitIsTapped)
+        return allBubbleIsTapped
     }
 
 }
