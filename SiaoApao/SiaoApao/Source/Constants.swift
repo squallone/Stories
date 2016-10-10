8c04677530214ee973a4eb1110072d5f0df633c5
Abdiel Soto <abdielsba@gmail.com>
2016-10-09 19:29:20 -0500

    - Pods, Caracteres VIew: Added Realm, Alamofire and Biography View

--- SiaoApao/SiaoApao/Source/Constants.swift
+++ SiaoApao/SiaoApao/Source/Constants.swift
@@ -3,11 +3,22 @@
 //  SiaoApao
 //
 //  Created by Abdiel Soto on 9/6/16.
-//  Copyright © 2016 Raúl López. All rights reserved.
+//  Copyright © 2016 SmartDev. All rights reserved.
 //
 
 import UIKit
 
-class Constants: NSObject {
+struct Constants{
     
+    struct Appdelegate {
+        static let Accessor = UIApplication.shared.delegate as! AppDelegate;
+    }
+    
+    struct HTTP {
+        static let Protocolo    = "https://"
+        static let Host         = "jsonblob.com/"
+        static let Port         = ""
+        static let Path         = "api/blob/"
+        static let Endpoint     = Constants.HTTP.Protocolo + Constants.HTTP.Host + Constants.HTTP.Port + Constants.HTTP.Path
+    }
 }
