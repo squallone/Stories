5bf95a68b62da41601c13d706f2091841c2aa7db
Raul Lopez Martinez <rlopezmartinez3@gmail.com>
2016-10-28 00:34:34 -0500

    - Dragon Scene: Fix Dragon Scene

--- SiaoApao/SiaoApao/Source/Project Classes/Scenes/SACarpetScene.swift
+++ SiaoApao/SiaoApao/Source/Project Classes/Scenes/SACarpetScene.swift
@@ -13,14 +13,13 @@ class SACarpetScene: SABaseScene {
     
     // MARK: - Properties
     var dragonNode: SKSpriteNode?
-    var carpetNode: SKSpriteNode?
     let stepNumber: Int = 7
     var numberOfTouches: Int = 5;
     var danceAction: SKAction!
     
     var backgroundMusicPlayer: AVAudioPlayer!
     let tiredSound: SKAction = SKAction.playSoundFileNamed(
-        "tired.wav", waitForCompletion: true)
+        "tired.wav", waitForCompletion: false)
     
     /* Setup your scene here */
     override func didMove(to view: SKView) {
@@ -30,7 +29,6 @@ class SACarpetScene: SABaseScene {
         
         /* Init properties */
         dragonNode = self.childNode(withName: "dragon") as? SKSpriteNode
-        carpetNode = self.childNode(withName: "carpet") as? SKSpriteNode
         
         /* Create Bounce Action */
         let bounceAction = SKAction.bounce(to: 1.005, duration: 0.2)
@@ -49,7 +47,6 @@ class SACarpetScene: SABaseScene {
         
         danceAction = SKAction.repeatForever(SKAction.sequence([groupAction,
                                                                 groupActionReversed]))
-        
     }
     
     // MARK: - Labels
@@ -67,6 +64,7 @@ class SACarpetScene: SABaseScene {
     // MARK: - Gesture Actions
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+
         if let location = touches.first?.location(in: self) {
             let touchedNode = atPoint(location)
             
@@ -79,37 +77,35 @@ class SACarpetScene: SABaseScene {
                         
                         self.playBackgroundMusic(filename: "dance.wav")
                         dragonNode?.run(danceAction, withKey: "dace")
-                        
-                        let actionVelocity = SKAction.wait(forDuration: 2.0)
-                        carpetNode?.run(actionVelocity, completion: {
-                            self.dragonNode?.isPaused = true
-                            self.backgroundMusicPlayer.pause()
-                        })
-
+                    
                     } else {
                         
+                        /* Other Touches */
                         if numberOfTouches > 0 {
-                            carpetNode?.removeAllActions()
+                            /* Resume Background Music */
                             backgroundMusicPlayer.play()
+                        
+                            /* Resume Dragon Dance Action */
+                            dragonNode?.isPaused = false
+                        
+                        } else {
+                            /* Stop background Sound */
+                            backgroundMusicPlayer.stop()
+                            
+                            /* Resume Action (VERY IMPORTANT) */
                             dragonNode?.isPaused = false
                             
-                            let actionVelocity =  SKAction.wait(forDuration: 2.0)
-                            carpetNode?.run(actionVelocity, completion: {
-                                self.dragonNode?.isPaused = true
-                                self.backgroundMusicPlayer.pause()
+                            /* Stop Dance Action*/
+                            let danceAction = dragonNode?.action(forKey: "dace")
+                            danceAction?.speed = 0.0;
+                            
+                            let finalImage = UIImage.init(named: "TiredDragon")
+                            let textureAction = SKAction.setTexture(SKTexture.init(image: finalImage!))
+                            let groupAction = SKAction.group([textureAction, tiredSound])
+                            dragonNode?.run(groupAction, completion: {
                                 
-                                self.numberOfTouches = self.numberOfTouches - 1
-                                
-                                if self.numberOfTouches == 0 {
-                                    //dragonNode?.removeAllActions()
-                                    let finalImage = UIImage.init(named: "TiredDragon")
-                                    self.dragonNode?.texture = SKTexture.init(image: finalImage!)
-                                    self.dragonNode?.run(self.tiredSound)
-                                    
-                                    // Show next button
-                                    self.showNextButton()
-                                    self.backgroundMusicPlayer.stop()
-                                }
+                                // Show next button
+                                self.showNextButton()
                             })
                         }
                     }
@@ -117,6 +113,20 @@ class SACarpetScene: SABaseScene {
             }
         }
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        /* Pause Dragon Dance Action */
+        self.dragonNode?.isPaused = true
+        
+        /* Puase Background Music */
+        self.backgroundMusicPlayer.pause()
+        
+        /* Decrement number of toches */
+        self.numberOfTouches -= 1
+    }
+    
     
     func playBackgroundMusic(filename: String) {
         let resourceUrl = Bundle.main.url(
